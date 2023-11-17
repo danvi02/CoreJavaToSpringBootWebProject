4787667060c59d92f28677f14a6695acb76d8904
Srinivas Basha <srinivas_basha@epam.com>
2023-11-17 13:14:57 +0530

    Committing code of Spring Boot Web

--- src/main/java/com/test/CoreJavaToSpringBootWebProject.java
+++ src/main/java/com/test/CoreJavaToSpringBootWebProject.java
@@ -1,9 +1,11 @@
 package com.test;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class CoreJavaToSpringBootWebProject{
+    public static void main(String[] args) {
+        SpringApplication.run(CoreJavaToSpringBootWebProject.class, args);
+    }
 }
